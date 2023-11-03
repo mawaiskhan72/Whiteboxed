eb578af88b6c7acee9527781f8c6a831c0ff897d
Muhammad Awais Khan <awaiskhann830@gmail.com>
2023-11-03 06:44:11 -0700

    code rebase

--- app/components/Properties.jsx
+++ app/components/Properties.jsx
@@ -7,26 +7,33 @@ function Properties() {
   return (
     <div>
       
-      <div className='flex justify-center pt-44 gap-[400px] bg-[#f9f9f9]'>
+      <div className='flex justify-center pt-44 gap-20 bg-[#f9f9f9]'>
+
       <div>
+
       <p className="font-bold font-family-Poppins text-[25px] ">Find properties that<br/>match your exact criteria</p>
       <p className='text-[#8b8b8b]'>Body text with explanations</p>
+      <Image src={Propertiesimg1}/>
       </div>
-      <div>
+        
+        <div>
       <p className="font-bold font-family-Poppins text-[25px]">Enhance Team Collaboration with<br/>Real-Time Visual Workflows</p>
-      <p className='text-[#8b8b8b]'>Body text with explanations</p>
-      </div>
-      </div>
-
-      <div className='flex justify-center bg-[#f9f9f9] gap-8 pt-10'>
-      <div>
-        <Image src={Propertiesimg1}/>
+      <p className='text-[#8b8b8b] mr-36'>Body text with explanations</p>
+      <Image src={Propertiesimg2}/>
       </div>
 
-      <div>
-        <Image src={Propertiesimg2}/>
       </div>
+
+       
+     <div>
+      <p class="text-black font-semibold font-family-Poppins  m-0 px-3 py-2">Body text with explanations</p>
+      <p class="text-secondary fs-6 fw-normal font-family-Poppins m-0 px-3 py-2">Body text with explanations</p>
+      <Image src={Whitebox.png}}/>
       </div>
+    
+     
+      
+
 
     </div>
   )
